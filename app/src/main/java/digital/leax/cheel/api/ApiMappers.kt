a510fb8bc852b82dee3870b5caa6fc710eca3a0a
Bip <adrien.grandpierre@edu.itescia.fr>
2021-06-16 18:17:00 +0200

    [Story 3 ] get que les lofi

--- app/src/main/java/digital/leax/cheel/api/ApiMappers.kt
+++ app/src/main/java/digital/leax/cheel/api/ApiMappers.kt
@@ -6,10 +6,17 @@ import digital.leax.cheel.Artist
 fun mapApiArtistWrapperToArtists(apiArtistsListWrapper: List<ApiArtists>): List<Artist> {
     val list = mutableListOf<Artist>()
     for (apiArtist in apiArtistsListWrapper) {
-        list.add(mapApiArtist(apiArtist))
+        if (genreIsLofi(apiArtist.genre_name)) {
+            list.add(mapApiArtist(apiArtist))
+        }
     }
     return list
 }
+
+fun genreIsLofi(genreName: String): Boolean {
+    return genreName.contains("lo-fi", true)
+}
+
 
 fun mapApiArtist(apiMovie: ApiArtists): Artist {
     return Artist(
